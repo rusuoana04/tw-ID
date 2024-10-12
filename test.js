a9d63ea2018cfa0e9ffa6bfe84c585b1d4e03550
Alexandru Olariu <rusuoana22@stud.ase.ro>
2024-10-12 11:20:13 +0300

    variables

---
+++ test.js
@@ -0,0 +1,29 @@
+function test() {
+    if(true)
+    {
+        var var1=1;
+        let let1=2;
+        const const1=3;
+    }
+    console.log(var1);
+    console.log(let1);
+    console.log(const1);
+
+}
+// test();
+
+let let2=2;
+let2=3;
+// console.log(let2);
+
+const obj = {}
+obj.name= "Adrian";
+
+// obj={}
+// console.log(obj);
+
+const arr=[1,2,3,4]
+arr.push(5);
+
+arr= [6,7,8];
+console.log(arr);
